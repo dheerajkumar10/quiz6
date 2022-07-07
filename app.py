682532deb38fe36a92d264731bf24e59dfd68a33
Dheeraj <dheerajkumart619@gmail.com>
2022-07-06 19:54:29 -0500

    assign6

--- app.py
+++ app.py
@@ -85,26 +85,51 @@ def student():
                 d = list(res2[0][2])
                 print(d)
                 print(days)
+                count = 0
                 for i in d:
                     if i in days:
                         print(i)
                         if days[i] < 2:
                             days[i] = days[i]+1
-                            dict['id'] = id
-                            dict['cname'] = res2[0][0]
-                            dict['section'] = res2[0][1]
-                            dict['days'] = res2[0][2]
-                            classl.append(dict)
+                            print(count)
+                            count = count+1
                     else:
                         print(i)
                         days[i] = 1
-                        dict['id'] = id
-                        dict['cname'] = res2[0][0]
-                        dict['section'] = res2[0][1]
-                        dict['days'] = res2[0][2]
-                        classl.append(dict)
+                        count = count+1
+                if(count > 0):
+                    dict['id'] = id
+                    dict['cname'] = res2[0][0]
+                    dict['section'] = res2[0][1]
+                    dict['days'] = res2[0][2]
+                    classl.append(dict)
 
     return render_template('student.html', data=classl)
+
+
+courselist = []
+di = {}
+
+
+@app.route('/admin', methods=['POST', 'GET'])
+def grade():
+    if request.method == "POST":
+        course = (request.form["course"])
+        section = (request.form["section"])
+        for i in range(len(classl)):
+            print(classl)
+            if(classl[i]['cname'] == course and classl[i]['section'] == section):
+                print("true")
+                query = "SELECT * FROM [dbo].[students] WHERE id=" + \
+                    classl[i]['id']
+                cursor.execute(query)
+                res = cursor.fetchall()
+                if res:
+                    di['id'] = res[0][0]
+                    di['name'] = res[0][1]
+                    di['age'] = res[0][3]
+                courselist.append(di)
+    return render_template('teacher.html', data=courselist)
 
 
 if __name__ == "__main__":
